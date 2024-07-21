b41b17730b166446bcf8ff5d39c7d2bf693283e9
Miguel <delgdevoleper@gmail.com>
2024-07-21 20:50:42 -0300

    Criando diretiva, v-for, v-focus

---
+++ src/components/data-binding.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="div1">
+      <input type="text" v-model="name">
+      <h1 :style="{ textDecoration: decoration }">Hello {{name}} !</h1>
+      <br>
+      <a :href="link">Acesse o github</a>
+    </div>
+  </template>
+  
+  <script>
+  export default{
+    data: ()=> ({
+      name: 'Miguel',
+      link: 'https://github.com',
+      decoration: 'underline'
+    })
+  }
+  </script>
+  
+  <style>
+  
+  .div1{
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, bold
+  }
+  </style>
+  
+  
--- src/components/diretivas-focus.vue
+++ src/components/diretivas-focus.vue
@@ -2,31 +2,31 @@
   <div>
     <h1>Diretivas</h1>
     <h2>Minha lista de tarefas!</h2>
-    <button @click="()=> showList = !showList">Ver minha lista!</button>
-    <br>
-    <input type="text" v-focus>
+    <button @click="() => (showList = !showList)">Ver minha lista!</button>
+    <br />
+    <input type="text" v-focus />
 
     <ul v-if="showList">
       <li v-for="(task, index) in tasks" :key="`${task.name}-${index}`">
-        {{ task.name}}
+        {{ task.name }}
       </li>
     </ul>
     <p v-else>Lista de tarefas escondidas</p>
   </div>
 </template>
-
-<script>
+  
+  <script>
 const focus = {
   mounted(el) {
     el.focus();
-  }
-}
+  },
+};
 
 export default {
   directives: {
-    focus
+    focus,
   },
-  name: 'DiretivasVue',
+  name: "DiretivasVue",
   data: () => ({
     showList: false,
     tasks: [{ name: "Fazer o Curso", isDone: false }],
