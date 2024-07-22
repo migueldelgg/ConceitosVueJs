27092dd996586b91e0961b6f54def51c64678ebe
Miguel <delgdevoleper@gmail.com>
2024-07-21 22:36:06 -0300

    Lifecycle hooks: doc

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="fonte">
+  <div class="fonte container">
     <dataBindingVue></dataBindingVue>
-    <p class="p">----------------------------------</p>
     <diretivasFocusVue></diretivasFocusVue>
-    <p class="p">----------------------------------</p>
     <eventosMetodosVue></eventosMetodosVue>
-    <p class="p">----------------------------------</p>
     <novaSyntaxsEAntigaVue></novaSyntaxsEAntigaVue>
-    <p class="p">----------------------------------</p>
+    <lifeCycleHooksVue></lifeCycleHooksVue>
   </div>
 </template>
 
@@ -15,31 +12,35 @@
 import dataBindingVue from "./components/data-binding.vue";
 import diretivasFocusVue from "./components/diretivas-focus.vue";
 import eventosMetodosVue from "./components/eventos-metodos.vue";
-import novaSyntaxsEAntigaVue from './components/nova-syntaxs-e-antiga.vue';
+import lifeCycleHooksVue from './components/life-cycle-hooks.vue';
+import novaSyntaxsEAntigaVue from "./components/nova-syntaxs-e-antiga.vue";
 
 export default {
   components: {
     dataBindingVue,
     diretivasFocusVue,
     eventosMetodosVue,
-    novaSyntaxsEAntigaVue
-  }
-}
+    novaSyntaxsEAntigaVue,
+    lifeCycleHooksVue
+  },
+};
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@800&display=swap');
 
-.p {
-  padding: 35px;
+.container {
+  display: flex;
+  justify-content: space-around; /* Distribui os componentes com espaço igual entre eles */
+  align-items: flex-start; /* Alinha os componentes no início do container */
+  flex-wrap: wrap; /* Permite que os componentes quebrem a linha se o espaço não for suficiente */
 }
 
-.fonte{
+.fonte {
   font-family: "Inter", sans-serif;
   font-optical-sizing: auto;
   font-weight: 800;
   font-style: normal;
-  font-variation-settings:
-    "slnt" 0;
+  font-variation-settings: "slnt" 0;
 }
 </style>
---
+++ src/components/life-cycle-hooks.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Lifecycle Hooks</h1>
+    <a
+      href="https://v3.vuejs.org/guide/composition-api-lifecycle-hooks.html#lifecycle-hooks"
+    >
+      Documentacao</a
+    >
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+/*
+export default{
+    mounted(){
+        console.log('foi montado!')
+    }
+}*/
+
+export default {
+  setup() {
+    onMounted(() => {
+      console.log("Foi montado com composion.api");
+      console.log(
+        "https://v3.vuejs.org/guide/composition-api-lifecycle-hooks.html#lifecycle-hooks"
+      );
+    });
+  },
+};
+</script>
